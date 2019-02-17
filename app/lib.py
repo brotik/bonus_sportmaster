11e2c60c0a873cbe5d7e696abdaab98af8d0a642
admin <brotik13@gmail>
2019-02-17 17:59:22 +0300

    initial commit

---
+++ app/lib.py
@@ -0,0 +1,32 @@
+def calculate_bonuses(accumulated, spent):
+    """
+    >>>calculate_bonuses(15000, 1000)
+    50
+    >>>calculate_bonuses(150_000, 1000)
+    70
+    >>>calculate_bonuses(200_000, 1000)
+    100
+    >>>calculate_bonuses(100, 20)
+    0
+    >>>calculate_bonuses(15_001, 3000)
+    210
+
+
+    :param accumulated:
+    :param spent:
+    :return:
+    """
+
+    bonus_cost = 1000
+
+    if spent < bonus_cost:
+        return 0
+    if 1 <= accumulated <= 15_000:
+        bonus = 50
+        if 15_000 < accumulated <= 150_000:
+            bonus = 70
+            if accumulated > 150_000:
+                bonus = 100
+    result = int(spent // bonus_cost) * bonus
+    return result
+
---
+++ app/main.py
@@ -0,0 +1,3 @@
+from lib import calculate_bonuses
+
+print(calculate_bonuses(100, 20))
